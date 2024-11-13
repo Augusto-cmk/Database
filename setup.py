a85a41a9c5cc73f27e1cdb8100c13021e9cfc34a
Augusto-cmk <pedromaia127@yahoo.com.br>
2024-11-13 18:22:47 -0300

    rewrite version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='database-event',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         *open('requirements.txt').read().split("\n")
