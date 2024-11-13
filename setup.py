19f01b2339a337b4df55c1071350e28ea12cdf5b
Augusto-cmk <pedromaia127@yahoo.com.br>
2024-11-13 15:38:28 -0300

    fix last version for multiple columns

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='database-event',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         *open('requirements.txt').read().split("\n")
